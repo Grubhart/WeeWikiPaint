ddb8a36dc505732adc5df8dc752008bff0fac6b0
edsonchavez@gmail.com <c7wnag3q>
2015-07-14 16:20:44 -0500

    Set up strict Lint options

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,40 @@
-task("default",["lint"]);
+/*global desc, task, jake, fail, complete*/
 
-desc("Lint everything");
-task("lint",[], function(){
-    var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js",{},{});
-   console.log("Lint code goes here");
-});
+(function() {
+    "use strict";
+
+    task("default", ["lint"]);
+
+    desc("Lint everything");
+    task("lint", [], function () {
+        var lint = require("./build/lib/lint_runner.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+
+        };
+    }
+}());
